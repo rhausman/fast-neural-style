44bc0bf3d9a0717582917066c393cdcc93401ea2
Richard Hausman <hausman.richard@gmail.com>
2020-12-29 23:40:03 -0500

    Fixed model download script

--- download_styling_models.sh
+++ download_styling_models.sh
@@ -1,2 +1,2 @@
-wget https://www.dropbox.com/s/gtwnyp9n49lqs7t/saved-models.zip?dl=1
-unzip saved-models.zip?dl=1
+wget "https://www.dropbox.com/s/gtwnyp9n49lqs7t/saved-models.zip?dl=0"
+unzip saved-models.zip?dl=0
