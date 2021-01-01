bdb34ebbca92f25181fcc692e45527dec327112e
Richard <hausman.richard@gmail.com>
2021-01-01 16:23:06 -0500

    Set up docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM python:3.9-slim-buster
+FROM python:3.8-slim-buster
 
 ENV VIRTUAL_ENV=/opt/venv
 # make the venv, and append to the PATH
-RUN python -m venv $VIRTUAL_ENV
+RUN python -m venv $VIRTUAL_ENV && pip install poetry
 ENV PATH="$VIRTUAL_ENV/bin:$PATH"
-COPY ./requirements.txt ./requirements.txt
-RUN pip install numpy
-RUN pip install -r "requirements.txt" --verbose
+RUN pip install torch
+COPY pyproject.toml pyproject.toml
+COPY poetry.lock poetry.lock
+RUN poetry install
 
 COPY . .
-
-ENTRYPOINT [ "uvicorn", "api:app", "--reload" ]
--- neural_style/neural_style.py
+++ neural_style/neural_style.py
@@ -22,14 +22,18 @@ def train(args):
 
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
-        kwargs = {'num_workers': 0, 'pin_memory': False}
+        kwargs = {"num_workers": 0, "pin_memory": False}
     else:
         kwargs = {}
 
-    transform = transforms.Compose([transforms.Scale(args.image_size),
-                                    transforms.CenterCrop(args.image_size),
-                                    transforms.ToTensor(),
-                                    transforms.Lambda(lambda x: x.mul(255))])
+    transform = transforms.Compose(
+        [
+            transforms.Scale(args.image_size),
+            transforms.CenterCrop(args.image_size),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255)),
+        ]
+    )
     train_dataset = datasets.ImageFolder(args.dataset, transform)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, **kwargs)
 
@@ -57,8 +61,8 @@ def train(args):
 
     for e in range(args.epochs):
         transformer.train()
-        agg_content_loss = 0.
-        agg_style_loss = 0.
+        agg_content_loss = 0.0
+        agg_style_loss = 0.0
         count = 0
         for batch_id, (x, _) in enumerate(train_loader):
             n_batch = len(x)
@@ -82,11 +86,13 @@ def train(args):
 
             content_loss = args.content_weight * mse_loss(features_y[1], f_xc_c)
 
-            style_loss = 0.
+            style_loss = 0.0
             for m in range(len(features_y)):
                 gram_s = Variable(gram_style[m].data, requires_grad=False)
                 gram_y = utils.gram_matrix(features_y[m])
-                style_loss += args.style_weight * mse_loss(gram_y, gram_s[:n_batch, :, :])
+                style_loss += args.style_weight * mse_loss(
+                    gram_y, gram_s[:n_batch, :, :]
+                )
 
             total_loss = content_loss + style_loss
             total_loss.backward()
@@ -97,18 +103,30 @@ def train(args):
 
             if (batch_id + 1) % args.log_interval == 0:
                 mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
-                    time.ctime(), e + 1, count, len(train_dataset),
-                                  agg_content_loss / (batch_id + 1),
-                                  agg_style_loss / (batch_id + 1),
-                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)
+                    time.ctime(),
+                    e + 1,
+                    count,
+                    len(train_dataset),
+                    agg_content_loss / (batch_id + 1),
+                    agg_style_loss / (batch_id + 1),
+                    (agg_content_loss + agg_style_loss) / (batch_id + 1),
                 )
                 print(mesg)
 
     # save model
     transformer.eval()
     transformer.cpu()
-    save_model_filename = "epoch_" + str(args.epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
-        args.content_weight) + "_" + str(args.style_weight) + ".model"
+    save_model_filename = (
+        "epoch_"
+        + str(args.epochs)
+        + "_"
+        + str(time.ctime()).replace(" ", "_")
+        + "_"
+        + str(args.content_weight)
+        + "_"
+        + str(args.style_weight)
+        + ".model"
+    )
     save_model_path = os.path.join(args.save_model_dir, save_model_filename)
     torch.save(transformer.state_dict(), save_model_path)
 
@@ -127,67 +145,145 @@ def check_paths(args):
 
 
 def stylize(args):
-    content_image = utils.tensor_load_rgbimage(args.content_image, scale=args.content_scale)
+    content_image = utils.tensor_load_rgbimage(
+        args.content_image, scale=args.content_scale
+    )
     content_image = content_image.unsqueeze(0)
 
     if args.cuda:
         content_image = content_image.cuda()
     content_image = Variable(utils.preprocess_batch(content_image), volatile=True)
+    print("loading net")
     style_model = TransformerNet()
     style_model.load_state_dict(torch.load(args.model))
-
+    print("loaded. Doing forward pass")
     if args.cuda:
         style_model.cuda()
 
     output = style_model(content_image)
+    print("Done")
     utils.tensor_save_bgrimage(output.data[0], args.output_image, args.cuda)
 
 
 def main():
-    main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
+    main_arg_parser = argparse.ArgumentParser(
+        description="parser for fast-neural-style"
+    )
     subparsers = main_arg_parser.add_subparsers(title="subcommands", dest="subcommand")
 
-    train_arg_parser = subparsers.add_parser("train",
-                                             help="parser for training arguments")
-    train_arg_parser.add_argument("--epochs", type=int, default=2,
-                                  help="number of training epochs, default is 2")
-    train_arg_parser.add_argument("--batch-size", type=int, default=4,
-                                  help="batch size for training, default is 4")
-    train_arg_parser.add_argument("--dataset", type=str, required=True,
-                                  help="path to training dataset, the path should point to a folder "
-                                       "containing another folder with all the training images")
-    train_arg_parser.add_argument("--style-image", type=str, default="images/style-images/mosaic.jpg",
-                                  help="path to style-image")
-    train_arg_parser.add_argument("--vgg-model-dir", type=str, required=True,
-                                  help="directory for vgg, if model is not present in the directory it is downloaded")
-    train_arg_parser.add_argument("--save-model-dir", type=str, required=True,
-                                  help="path to folder where trained model will be saved.")
-    train_arg_parser.add_argument("--image-size", type=int, default=256,
-                                  help="size of training images, default is 256 X 256")
-    train_arg_parser.add_argument("--style-size", type=int, default=None,
-                                  help="size of style-image, default is the original size of style image")
-    train_arg_parser.add_argument("--cuda", type=int, required=True, help="set it to 1 for running on GPU, 0 for CPU")
-    train_arg_parser.add_argument("--seed", type=int, default=42, help="random seed for training")
-    train_arg_parser.add_argument("--content-weight", type=float, default=1.0,
-                                  help="weight for content-loss, default is 1.0")
-    train_arg_parser.add_argument("--style-weight", type=float, default=5.0,
-                                  help="weight for style-loss, default is 5.0")
-    train_arg_parser.add_argument("--lr", type=float, default=1e-3,
-                                  help="learning rate, default is 0.001")
-    train_arg_parser.add_argument("--log-interval", type=int, default=500,
-                                  help="number of images after which the training loss is logged, default is 500")
+    train_arg_parser = subparsers.add_parser(
+        "train", help="parser for training arguments"
+    )
+    train_arg_parser.add_argument(
+        "--epochs", type=int, default=2, help="number of training epochs, default is 2"
+    )
+    train_arg_parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=4,
+        help="batch size for training, default is 4",
+    )
+    train_arg_parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        help="path to training dataset, the path should point to a folder "
+        "containing another folder with all the training images",
+    )
+    train_arg_parser.add_argument(
+        "--style-image",
+        type=str,
+        default="images/style-images/mosaic.jpg",
+        help="path to style-image",
+    )
+    train_arg_parser.add_argument(
+        "--vgg-model-dir",
+        type=str,
+        required=True,
+        help="directory for vgg, if model is not present in the directory it is downloaded",
+    )
+    train_arg_parser.add_argument(
+        "--save-model-dir",
+        type=str,
+        required=True,
+        help="path to folder where trained model will be saved.",
+    )
+    train_arg_parser.add_argument(
+        "--image-size",
+        type=int,
+        default=256,
+        help="size of training images, default is 256 X 256",
+    )
+    train_arg_parser.add_argument(
+        "--style-size",
+        type=int,
+        default=None,
+        help="size of style-image, default is the original size of style image",
+    )
+    train_arg_parser.add_argument(
+        "--cuda",
+        type=int,
+        required=True,
+        help="set it to 1 for running on GPU, 0 for CPU",
+    )
+    train_arg_parser.add_argument(
+        "--seed", type=int, default=42, help="random seed for training"
+    )
+    train_arg_parser.add_argument(
+        "--content-weight",
+        type=float,
+        default=1.0,
+        help="weight for content-loss, default is 1.0",
+    )
+    train_arg_parser.add_argument(
+        "--style-weight",
+        type=float,
+        default=5.0,
+        help="weight for style-loss, default is 5.0",
+    )
+    train_arg_parser.add_argument(
+        "--lr", type=float, default=1e-3, help="learning rate, default is 0.001"
+    )
+    train_arg_parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=500,
+        help="number of images after which the training loss is logged, default is 500",
+    )
 
-    eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
-    eval_arg_parser.add_argument("--content-image", type=str, required=True,
-                                 help="path to content image you want to stylize")
-    eval_arg_parser.add_argument("--content-scale", type=float, default=None,
-                                 help="factor for scaling down the content image")
-    eval_arg_parser.add_argument("--output-image", type=str, required=True,
-                                 help="path for saving the output image")
-    eval_arg_parser.add_argument("--model", type=str, required=True,
-                                 help="saved model to be used for stylizing the image")
-    eval_arg_parser.add_argument("--cuda", type=int, required=True,
-                                 help="set it to 1 for running on GPU, 0 for CPU")
+    eval_arg_parser = subparsers.add_parser(
+        "eval", help="parser for evaluation/stylizing arguments"
+    )
+    eval_arg_parser.add_argument(
+        "--content-image",
+        type=str,
+        required=True,
+        help="path to content image you want to stylize",
+    )
+    eval_arg_parser.add_argument(
+        "--content-scale",
+        type=float,
+        default=None,
+        help="factor for scaling down the content image",
+    )
+    eval_arg_parser.add_argument(
+        "--output-image",
+        type=str,
+        required=True,
+        help="path for saving the output image",
+    )
+    eval_arg_parser.add_argument(
+        "--model",
+        type=str,
+        required=True,
+        help="saved model to be used for stylizing the image",
+    )
+    eval_arg_parser.add_argument(
+        "--cuda",
+        type=int,
+        required=True,
+        help="set it to 1 for running on GPU, 0 for CPU",
+    )
 
     args = main_arg_parser.parse_args()
 
