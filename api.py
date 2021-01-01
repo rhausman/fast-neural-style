0b53709db55a785607fe34343e0831b19f1a9d0c
Richard <hausman.richard@gmail.com>
2021-01-01 16:22:49 -0500

    Added style param to api

--- api.py
+++ api.py
@@ -3,10 +3,22 @@ from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+#from app.api.routes import router as api_router
 
 # to run server, uvicorn api:app --reload
-app = FastAPI()
-
+def get_application():
+    app = FastAPI(title="Phresh", version="1.0.0")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    #app.include_router(api_router, prefix="/api")
+    return app
+app = get_application()
 
 class Item(BaseModel):
     name: str
@@ -15,7 +27,29 @@ class Item(BaseModel):
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file(
+    file: UploadFile = File(...), style: Optional[str] = "mosaic"
+):
+    # resolve style
+    if style + ".pth" not in os.listdir("saved-models"):
+        style = "mosaic"
+
+    contents = await file.read()
+    with open("tmp/input.jpg", "wb") as ff:
+        ff.write(contents)
+
+    os.system(
+        "python neural_style/neural_style.py eval --content-image tmp/input.jpg --model saved-models/"
+        + style
+        + ".pth --output-image tmp/output.jpg  --cuda 0"
+    )
+    ##processing
+
+    return {"style": style, "img": FileResponse("tmp/output.jpg")}
+
+
+@app.get("/style_image/{style}")
+async def stylize_image(style: str, file: UploadFile = File(...)):
     contents = await file.read()
     with open("tmp/input.jpg", "wb") as ff:
         ff.write(contents)
@@ -25,7 +59,7 @@ async def create_upload_file(file: UploadFile = File(...)):
     )
     ##processing
 
-    return FileResponse("tmp/output.jpg")
+    return {"style": style, "img": FileResponse("tmp/output.jpg")}
 
 
 @app.get("/")
